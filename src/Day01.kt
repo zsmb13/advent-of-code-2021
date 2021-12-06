463dced0b551be07f4257513e7e94589ab4b2eeb
Márton Braun <braunmarci@gmail.com>
2021-12-06 14:24:32 +0100

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map(String::toInt)
+            .windowed(2)
+            .count { (a, b) -> a < b }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map(String::toInt)
+            .windowed(3)
+            .map(List<Int>::sum)
+            .windowed(2)
+            .count { (a, b) -> a < b }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_Test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
